04c4466503ee9a414c5e1bf0f60af2a73e99fbc5
Elwin Daniel <elwindaniel13@gmail.com>
2025-07-13 12:44:05 +0100

    5:30 perordeer

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import { FaPhoneAlt, FaEnvelope } from 'react-icons/fa';
+import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 import Sponsors from "@/components/sponsors";
 import UserForm from "@/components/userForm";
 import "@/styles/home.css";
@@ -26,8 +26,8 @@ export default function Home() {
             Welcome to <b>Aaravam 3.0</b>, an extravagant celebration of Onam,
             happening live on{" "}
             <b>
-              September 6, 2025, at Stilleweg 12B, 30655, Hannover. Starting at
-              11:00 AM,
+              September 6, 2025, at walderseestraße 100 ,30177, Hannover.
+              Starting at 11:00 AM,
             </b>{" "}
             this event promises to be a day filled with vibrant cultural
             performances, exciting traditional games, and a delicious grand
@@ -52,8 +52,9 @@ export default function Home() {
             biggest Onam celebration in Hannover!
           </p>
           <div class="map-container">
+           
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2434.146723269625!2d9.8184785!3d52.404016999999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b00cf571f7d019%3A0x74cac7abddfe86da!2sStilleweg%2012B%2C%2030655%20Hannover%2C%20Germany!5e0!3m2!1sen!2suk!4v1723248505980!5m2!1sen!2suk"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2434.93246446518!2d9.755480199999997!3d52.389774599999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b07354b8bb7b97%3A0xa6d56ef46b88b5fc!2sWalderseestra%C3%9Fe%20100%2C%2030177%20Hannover%2C%20Germany!5e0!3m2!1sen!2suk!4v1752406883148!5m2!1sen!2suk"
               allowfullscreen=""
               loading="lazy"
               referrerpolicy="no-referrer-when-downgrade"
@@ -77,39 +78,40 @@ export default function Home() {
               height={500}
             />
           </div>
-          <div className="mobi home-about-event-content-right-inner">
+          {/* <div className="mobi home-about-event-content-right-inner">
             <div className="box-title">Our Sponsors</div>
             <Sponsors />
-          </div>
+          </div> */}
         </div>
       </section>
-      <div className="dek home-about-event-content-right-inner">
+      {/* <div className="dek home-about-event-content-right-inner">
         <div className="box-title">Our Sponsors</div>
         <Sponsors />
-      </div>
+      </div> */}
       <section className="home-about-event-content">
-      <div className="box-title">For Queries</div>
-      <div className="contact-box">
-
-      <div className="contact-info">
-        <div className="contact-item">
-          <FaPhoneAlt className="icon" />
-          <a href="tel:+491785575267">Midhun: +49 1785575267</a>
+        <div className="box-title">For Queries</div>
+        <div className="contact-box">
+          <div className="contact-info">
+            <div className="contact-item">
+              <FaPhoneAlt className="icon" />
+              <a href="tel:+491785575267">Midhun: +49 1785575267</a>
+            </div>
+            <div className="contact-item">
+              <FaPhoneAlt className="icon" />
+              <a href="tel:+4915510534195">Shyam: +49 15510534195</a>
+            </div>
+            <div className="contact-item">
+              <FaPhoneAlt className="icon" />
+              <a href="tel:+491794360268">Aswathy: +49 176 55790400</a>
+            </div>
+            <div className="contact-item email">
+              <FaEnvelope className="icon" />
+              <a href="mailto:Hannovermalayalis@gmail.com">
+                Email: Hannovermalayalis@gmail.com
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="contact-item">
-          <FaPhoneAlt className="icon" />
-          <a href="tel:+4915510534195">Shyam: +49 15510534195</a>
-        </div>
-        <div className="contact-item">
-          <FaPhoneAlt className="icon" />
-          <a href="tel:+491794360268">Sijo: +49 179 4360268</a>
-        </div>
-        <div className="contact-item email">
-          <FaEnvelope className="icon" />
-          <a href="mailto:Hannovermalayalis@gmail.com">Email: Hannovermalayalis@gmail.com</a>
-        </div>
-      </div>
-      </div>
       </section>
     </div>
   );
